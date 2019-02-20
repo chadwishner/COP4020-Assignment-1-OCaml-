b6bfeb4d873c6d5148392ce7e844aa31e573f46c
Chad Wishner <chadwishner@gmail.com>
2019-02-19 22:10:57 -0500

    Resubmission

--- rpn.ml
+++ rpn.ml
@@ -1,31 +1,65 @@
-(*Chad Wishner 66315393*)
+type token = 
+  | Num of float
+  | Op of string
+  | Terror of string
 
-let rpn_evl s =
-  let calculate op = function
-    | a::b::c -> (op b a)::c (* Use the OCaml native operations to do math *)
-    | _ -> invalid_arg "Not a valid expression" in
-      (* Match the operations and call calculate function *)
-      let operators o = function 
-        | "*" -> calculate( *. ) o
-        | "/" -> calculate( /. ) o
-        | "+" -> calculate( +. ) o
-        | "-" -> calculate( -. ) o
-        | "^" -> calculate( ** ) o
-        | str -> (float_of_string str) :: o in (* Convert string to float where necessary *)
-        let list = Str.split(Str.regexp " +") s in (* Convert the input string to a list of strings *) 
-          List.fold_left operators [] list (* Operate on list pre-application and store in accumulator *)
+let toToken (str: string) : token = 
+      match str with
+      | "+" | "-" | "*" | "/" -> Op(str)
+      | _ -> let f=float_of_string_opt(str) in 
+          match f with
+              | Some(flt) -> Num(flt)
+              | None -> Terror(str)
 
-(* Printing the result and calling the evaluation function *)
-let do_stuff input = 
-  print_string("Result: "); 
-  print_float(List.hd(rpn_evl input)); (* Print the first element in the list after doing evaluations on the string input *)
-  print_newline();;
+type state =
+  | MyStack of float list
+  | Serror of string 
 
-(* Recursive function to take recurring input *)
+let evalOp (s: string) (op1: float) (op2: float) : float =
+	match s with
+    | "+" -> op1 +. op2
+    | "-" -> op1 -. op2
+    | "*" -> op1 *. op2
+    | "/" -> op1 /. op2
+    | "^" -> op1 ** op2
+		| _ -> 0.0
+
+let nextState (st: state) (t: token) : state =
+  match st with
+    | Serror(str) -> st
+    | MyStack(lst) -> 
+      match t with
+        | Num(f) -> MyStack(f :: lst)
+        | Op(s) -> (
+          match lst with
+            | op2::op1::tail -> MyStack( evalOp s op1 op2 :: tail)
+            | _ -> Serror("Not enough arguments for " ^ s)
+        )
+        | Terror(s) -> Serror("Unknown token " ^ s)
+
+let finalState state =
+  match state with
+  | MyStack(stack) -> (
+    match stack with
+      | op::[] -> (print_float(op);
+        print_newline();
+      )
+      | _ -> print_string("Not enough operators"); 
+    )
+  | Serror(err) -> print_string(err);;
+
+let procRPN str =
+  str |> 
+    String.split_on_char ' ' |> 
+      List.map toToken |>
+        List.fold_left nextState (MyStack []) |>
+          finalState;;
+
+  
 let rec exe () =  
   match read_line () with
     | "stop" -> print_endline "Ending Program" (* End Program *)
-    | _ as input -> let () = do_stuff input in exe();; (* Take input in, Call function for printing, recursively call exe *)   
-
+    | _ as input -> let () = procRPN input in exe();; (* Take input in, Call function for printing, recursively call exe *)   
+    
 (* Start Program*)
 exe ();;
---
+++ rpn_old.ml
@@ -0,0 +1,31 @@
+(*Chad Wishner 66315393*)
+
+let rpn_evl s =
+  let calculate op = function
+    | a::b::c -> (op b a)::c (* Use the OCaml native operations to do math *)
+    | _ -> invalid_arg "Not a valid expression" in
+      (* Match the operations and call calculate function *)
+      let operators o = function 
+        | "*" -> calculate( *. ) o
+        | "/" -> calculate( /. ) o
+        | "+" -> calculate( +. ) o
+        | "-" -> calculate( -. ) o
+        | "^" -> calculate( ** ) o
+        | str -> (float_of_string str) :: o in (* Convert string to float where necessary *)
+        let list = Str.split(Str.regexp " +") s in (* Convert the input string to a list of strings *) 
+          List.fold_left operators [] list (* Operate on list pre-application and store in accumulator *)
+
+(* Printing the result and calling the evaluation function *)
+let do_stuff input = 
+  print_string("Result: "); 
+  print_float(List.hd(rpn_evl input)); (* Print the first element in the list after doing evaluations on the string input *)
+  print_newline();;
+
+(* Recursive function to take recurring input *)
+let rec exe () =  
+  match read_line () with
+    | "stop" -> print_endline "Ending Program" (* End Program *)
+    | _ as input -> let () = do_stuff input in exe();; (* Take input in, Call function for printing, recursively call exe *)   
+
+(* Start Program*)
+exe ();;
